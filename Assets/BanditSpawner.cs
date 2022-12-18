727c8a276e9bb172ce989da3137b9855dc63e988
cugi143 <cugino.macuga@gmail.com>
2022-12-18 02:00:31 +0100

    Merge

--- Assets/BanditSpawner.cs
+++ Assets/BanditSpawner.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class BanditSpawner : MonoBehaviour
 {
     public GameObject Bandit;
+    public GameObject Target;
+    public GameObject Player;
 
     public GameObject Spawnpoint;
+
+    public int WaveNumber = 1;
     // Start is called before the first frame update
     void Start()
     {
+        int BanditCount = WaveNumber * 2 + 5;
         for (int i = 0; i < 5; i++)
         {
             Vector3 pos = Spawnpoint.transform.position;
